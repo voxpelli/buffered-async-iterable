c837641390f0a7b66afe1435209a347458fbcf1c
Pelle Wessman <pelle@kodfabrik.se>
2021-05-25 18:12:48 +0200

    Added todos

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 /// <reference types="node" />
 
 // FIXME: Check this https://twitter.com/matteocollina/status/1392056117128306691
+// FIXME: Read up on https://tc39.es/ecma262/#table-async-iterator-optional and add return() and throw(). return() is called by a "for await" when eg. a "break" or a "throw" happens within it
+// TODO: Have option to persist order? To not use Promise.race()?
 // TODO: Make a proper merge for async iterables by accepting multiple input iterables, see: https://twitter.com/matteocollina/status/1392056092482576385
 
 /**
@@ -23,10 +25,12 @@ const bufferAsyncIterable = (asyncIterable, callback, size = 3) => {
   /** @type {Set<NextLookup>} */
   const bufferedPromises = new Set();
 
+  // TODO: Check if it's already an async iterator?
   const asyncIterator = asyncIterable[Symbol.asyncIterator]();
 
   const queueNext = () => {
     // console.log('😳 queueNext', Date.now());
+    // FIXME: Handle rejected promises from upstream! And properly mark this iterator as completed
     /** @type {NextLookup} */
     const next = asyncIterator.next()
       // eslint-disable-next-line promise/prefer-await-to-then
@@ -35,6 +39,7 @@ const bufferAsyncIterable = (asyncIterable, callback, size = 3) => {
         ...(
           result.done
             ? result
+            // FIXME: Handle rejected promises from callback! And properly mark this iterator as completed + the upstream one
             // eslint-disable-next-line promise/no-callback-in-promise
             : { value: await callback(result.value) }
         )
@@ -54,6 +59,7 @@ const bufferAsyncIterable = (asyncIterable, callback, size = 3) => {
       return { done: true, value: undefined };
     }
 
+    // FIXME: Handle rejected promises!
     const { bufferPromise, ...result } = await Promise.race(bufferedPromises);
     // console.log('🏎 race has been won!', result, 'at time', Date.now());
 
