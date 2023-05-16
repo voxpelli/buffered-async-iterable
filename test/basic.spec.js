38fc40fd905fea02a85effe36f4a28eaee78c271
Pelle Wessman <pelle@kodfabrik.se>
2023-05-16 17:20:48 +0200

    Split tests apart

---
+++ test/basic.spec.js
@@ -0,0 +1,131 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import chaiQuantifiers from 'chai-quantifiers';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import {
+  map as bufferAsyncIterable,
+} from '../index.js';
+
+chai.use(chaiAsPromised);
+chai.use(chaiQuantifiers);
+chai.use(sinonChai);
+
+const should = chai.should();
+
+describe('bufferAsyncIterable() basic', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should throw on falsy asyncIterable argument', () => {
+    should.Throw(() => {
+      // @ts-ignore
+      bufferAsyncIterable();
+    }, TypeError, 'Expected input to be provided');
+  });
+
+  it('should throw when provided asyncIterable is not an asyncIterable', () => {
+    should.Throw(() => {
+      // @ts-ignore
+      bufferAsyncIterable(true);
+    }, TypeError, 'Expected asyncIterable to have a Symbol.asyncIterator function');
+  });
+
+  it('should throw when provided callback is not a function', () => {
+    should.Throw(() => {
+      const asyncIterable = (async function * () {})();
+      bufferAsyncIterable(
+        asyncIterable,
+        // @ts-ignore
+        { queueSize: true }
+      );
+    }, TypeError, 'Expected callback to be a function');
+  });
+
+  it('should throw when provided size is not a number', () => {
+    should.Throw(() => {
+      const asyncIterable = (async function * () {})();
+      bufferAsyncIterable(
+        asyncIterable,
+        async () => {},
+        // @ts-ignore
+        { queueSize: true }
+      );
+    }, TypeError, 'Expected queueSize to be a number');
+  });
+
+  it('should return an AsyncIterable when provided with required arguments', () => {
+    const asyncIterable = (async function * () {})();
+    const bufferedAsyncIterable = bufferAsyncIterable(
+      asyncIterable,
+      async () => {}
+    );
+
+    should.exist(bufferedAsyncIterable);
+    bufferedAsyncIterable.should.be.an('object');
+
+    should.exist(bufferedAsyncIterable[Symbol.asyncIterator]);
+    bufferedAsyncIterable[Symbol.asyncIterator].should.be.a('function');
+  });
+
+  it('should return an AsyncIterable when provided with all arguments', () => {
+    const asyncIterable = (async function * () {})();
+    const bufferedAsyncIterable = bufferAsyncIterable(
+      asyncIterable,
+      async () => {},
+      { queueSize: 10 }
+    );
+
+    should.exist(bufferedAsyncIterable);
+    bufferedAsyncIterable.should.be.an('object');
+
+    should.exist(bufferedAsyncIterable[Symbol.asyncIterator]);
+    bufferedAsyncIterable[Symbol.asyncIterator].should.be.a('function');
+  });
+
+  it('should return an AsyncIterable when provided with an array value', () => {
+    const bufferedAsyncIterable = bufferAsyncIterable(
+      ['a', 'b', 'c'],
+      async () => {}
+    );
+
+    should.exist(bufferedAsyncIterable);
+    bufferedAsyncIterable.should.be.an('object');
+
+    should.exist(bufferedAsyncIterable[Symbol.asyncIterator]);
+    bufferedAsyncIterable[Symbol.asyncIterator].should.be.a('function');
+  });
+
+  it('should return an AsyncIterable when provided with a Set value', () => {
+    const bufferedAsyncIterable = bufferAsyncIterable(
+      new Set(['a', 'b', 'c']),
+      async () => {}
+    );
+
+    should.exist(bufferedAsyncIterable);
+    bufferedAsyncIterable.should.be.an('object');
+
+    should.exist(bufferedAsyncIterable[Symbol.asyncIterator]);
+    bufferedAsyncIterable[Symbol.asyncIterator].should.be.a('function');
+  });
+
+  it('should return an AsyncIterable when provided when chained with itself', () => {
+    const asyncIterable = (async function * () {})();
+    const chainedBufferedAsyncIterable = bufferAsyncIterable(
+      asyncIterable,
+      async () => {}
+    );
+    const bufferedAsyncIterable = bufferAsyncIterable(
+      chainedBufferedAsyncIterable,
+      async () => {}
+    );
+
+    should.exist(bufferedAsyncIterable);
+    bufferedAsyncIterable.should.be.an('object');
+
+    should.exist(bufferedAsyncIterable[Symbol.asyncIterator]);
+    bufferedAsyncIterable[Symbol.asyncIterator].should.be.a('function');
+  });
+});
